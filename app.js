6b34445eb899a6864f95eb0f87dc79c4d4894b18
Khulan <khulan.msc.2012@gmail.com>
2016-06-23 19:02:20 +0200

    Khulan Login credentials and job lists

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var session = require('express-session');
 var util = require('util');
 var request = require('request');
 var Cloudant = require('cloudant');
@@ -19,6 +20,8 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -36,10 +39,10 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 
 
 
+
 var dbURL = "https://241d47a6-cc56-4f32-b4ca-e74d969d5768-bluemix:4f08adeee6f4f8f53968fb40b57d06052e6ae66cce96ee1375d567de811dea49@241d47a6-cc56-4f32-b4ca-e74d969d5768-bluemix.cloudant.com";
 
 app.get('/companyData', function(req, res) {
-	var companies = {"companies":[]};
 	 request.get({
 	        url: dbURL +"/company/_design/companies/_view/companies",
 	        headers: {
@@ -50,8 +53,6 @@ app.get('/companyData', function(req, res) {
 	        	}
 	        	else {
 	        		if((response.statusCode < 300)) {
-	        			console.log("=== body ===");
-	        			console.log(body);
 	        			res.send(JSON.parse(body).rows);
 	        		} else {
 	        			console.log("We have no error, but status code is not valid: "+response.statusCode);
@@ -62,7 +63,6 @@ app.get('/companyData', function(req, res) {
 });
 
 app.get('/jobData', function(req, res) {
-	var companies = {"jobs":[]};
 	 request.get({
 	        url: dbURL +"/job/_design/jobs/_view/jobs",
 	        headers: {
@@ -73,7 +73,6 @@ app.get('/jobData', function(req, res) {
 	        	}
 	        	else {
 	        		if((response.statusCode < 300)) {
-	        			console.log("=== body ===");
 	        			res.send(JSON.parse(body).rows);
 	        		} else {
 	        			console.log("We have no error, but status code is not valid: "+response.statusCode);
@@ -84,68 +83,35 @@ app.get('/jobData', function(req, res) {
 });
 
 //LOGIN
-app.get('/login', function(req, res) {	
-	var companies = {"companies":[]};
-	 request.get({
-	        url: dbURL +"/user/_all_docs",
-	        headers: {
-	            'Content-Type': 'application/json'
-	        }}, function (error, response, body) {
-	        	if ((error) || (!response.statusCode)) {
-	        		console.log("ERROR: Can't get target db's docs." +error);
-	        	}
-	        	else {
-	        		if((response.statusCode < 300)) {
-	        			
-	        			 var d = {};
-	        			// else d = { "success":false; "nextUrl":""};
-	        			
-	        			users =JSON.parse(body).rows;
-	        			for(i=0; i<users.length; i++){
-	        				user_id = users[i].id;
-	        				request.get({
-	        			        url: dbURL +"/user/"+user_id,
-	        			        headers: {
-	        			            'Content-Type': 'application/json'
-	        			        }}, function(error, response, reqBody){
-	        			        	var user = JSON.parse(reqBody).userName;	        			        	
-	        			        	var password = crypto.createHash('md5').update(req.param('password')).digest('hex');
-
-	        			        	console.log(password);	        			        	
-	        			        	if(user == req.param('userName') && password ==  JSON.parse(reqBody).password){
-	        			        		d = { "success":true, "nextUrl":"/job.html"};
-	        			        		res.send(d);
-	        			        	}
-	        				});
-	        			}
-	        			
-	        		} else {
-	        			console.log("We have no error, but status code is not valid: "+response.statusCode);
-	        		}
-	        	}
-	        }
-	    );
+app.post('/login', function(req, res) {	
+	var userName = req.body.userName;
+	var tempPass = req.body.password;
+	var password = crypto.createHash('md5').update(tempPass).digest('hex');
+	var d = {"success":false, "nextUrl":""};
 	
+	request.get({
+		url: dbURL +"/user/_design/user/_view/user?userName="+userName+"&"+"password="+password,
+	}, function(error, response, body){
+		if(error){
+			console.log("Error occurred during login");			
+			res.send(d)
+		}
+		else{
+			var result = JSON.parse(body).rows.length;
+			if(result>0){
+				d = { "success":true, "nextUrl":"/job.html"};
+				res.send(d);
+			}
+			else{
+				res.send(d);
+			}
+		}
+	});
 	
-//	var str = {"selector" : {"username": req.param('userName')}};
-//	console.log(str);
-//	request.post({
-//        url: dbURL +"/user/_find",
-//        headers: {
-//            'Content-Type': 'application/json'
-//        },
-//        body: str}, function(error, response, body){
-//        	if(error){
-//        		console.log("Error retrieving user credentials");
-//        	}
-//        	else{
-//        		res.send(body);
-//        	}		
-//	})
 });
 
+//LOGOUT
 app.get('/logout', function(req, res) {
-	delete req.session.user_id;
 	res.redirect('/login');
 });
 
