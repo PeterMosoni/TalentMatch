d92123bac71c890f8b14d30299d1e83365a9906d
Khulan <khulan.msc.2012@gmail.com>
2016-06-24 09:15:41 +0200

    Khulan>>>Ordering ratings in descending order

--- app.js
+++ app.js
@@ -171,6 +171,7 @@ app.post('/candidatesOnJobRating', function(req, res) {
 		getCandidateDataForRating(ratingArray, function(dataArray){
 			//orderData(dataArray); ???????
 			dataArray = { result: dataArray }
+			dataArray = orderRatingArray(dataArray);
 			res.send(dataArray);
 		});		
 	});
@@ -256,6 +257,22 @@ function fillOneDataItemWithCandidateData(dataItem, callback){
     );
 }
 
+function orderRatingArray(dataArray){
+	var ratingResults = dataArray.result;
+	var sort_by = function(field, reverse, primer){
+
+		   var key = primer ? 
+		       function(x) {return primer(x[field])} : 
+		       function(x) {return x[field]};
+
+		   reverse = !reverse ? 1 : -1;
+
+		   return function (a, b) {
+		       return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+		     } 
+		}
+	return ratingResults.sort(sort_by('rating', false, parseFloat));
+}
 
 
 //-----------------
